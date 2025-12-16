863f3c4907bae8bb694a47577d81a3ad1db76c27
Natouille04 <nathan.gouveia2008@gmail.com>
2025-12-16 17:25:53 +0100

    Fixing AGAIN a syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 import Home from "./pages/Home.jsx";
 import Login from "./pages/auth/Login.jsx"
